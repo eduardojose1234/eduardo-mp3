db75ff22a871e8a799f3532d9f56cf97cd9c9d7f
eduardo jose <arismendyguerreroe@gmail.com>
2024-08-22 18:48:13 -0400

    first commit

---
+++ music.js
@@ -0,0 +1,14 @@
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play2");
+
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+    audio.play();
+  } else {
+    audio.pause();
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
